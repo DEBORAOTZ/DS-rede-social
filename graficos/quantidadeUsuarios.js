11987dc7498b9324a6a12edfffed405de2b74e96
DEBORAOTZ <132685907+DEBORAOTZ@users.noreply.github.com>
2024-10-16 08:03:50 -0300

    aula10

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -18,9 +18,18 @@ async function quantidadeUsuarios() {
     }
   ]
 
-  const layout = {
-    plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color')
+  
+  
+const layout = {
+  plot_bgcolor: getCSS('--bg-color'),
+  paper_bgcolor: getCSS('--bg-color'),
+  title: {
+    text: 'Redes sociais com mais usuários no mundo',
+     x: 0,
+     font: {
+         color: getCSS('--primary-color'),
+         family: getCSS('--font'),
+         size: 30
   }
 
   const grafico = document.createElement('div')
@@ -28,6 +37,18 @@ async function quantidadeUsuarios() {
   document.getElementById('graficos-container').appendChild(grafico)
   Plotly.newPlot(grafico, data, layout)
 
+ 
+  
+
+
+ 
+  
+  
+ 
 }
+
+
+  
+    
 
 quantidadeUsuarios()
